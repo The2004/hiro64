aecb3ac8b17ad49e2912210bab58afe88e79cc69
2004 <71972488+The2004@users.noreply.github.com>
2023-03-21 00:02:17 +0200

    Add files via upload

--- hiro64.py
+++ hiro64.py
@@ -1,38 +1,36 @@
 import base64
 import math
-from shutil import ExecError
-chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789="
 to = chars
 def hash(plaintext):
     hashed = 1;
-    btext = base64.b64encode(plaintext.encode("ascii")).decode("ascii").replace("=","")
+    btext = base64.b64encode(plaintext.encode("utf-8")).decode("utf-8").replace("=","")
     for char in btext:
         hashed += 2 * (chars.rfind(str(char)) + 1) - (chars.rfind(str(char)) + 1) * 0.25
     return int(hashed)
 
 def encrypt(plaintext, key):
     global to
-    if type(key) != int:
-        raise Exception("given key must be integer. Use hash() funtion if you want to use strigs as keys")
-    btext = base64.b64encode(plaintext.encode("ascii")).decode("ascii")
+    btext = base64.b64encode(plaintext.encode("utf-8")).decode("utf-8") 
+    encodedkey = base64.b64encode(key.encode("utf-8")).decode("utf-8") 
     for i in range(len(to)):
-        chosen = to[customrandom(0, len(to), key + i)]
+        chosen = to[customrandom(0, len(to), hash(encodedkey[customrandom(0, len(encodedkey), i)]))]
         to = to.replace(chosen, "")
         to = chosen + to
     cip = ''
     for ch in btext:
         cip += to[chars.rfind(ch)]
+    print(to)
     to = chars
     
     return cip
 
 def decrypt(ciphertext, key):
     global to
-    if type(key) != int:
-        raise Exception("given key must be integer. Use hash() funtion if you want to use strigs as keys")
     btext = ciphertext
+    encodedkey = base64.b64encode(key.encode("utf-8")).decode("utf-8") 
     for i in range(len(to)):
-        chosen = to[customrandom(0, len(to), key + i)]
+        chosen = to[customrandom(0, len(to), hash(encodedkey[customrandom(0, len(encodedkey), i)]))]
         to = to.replace(chosen, "")
         to = chosen + to
     plaintext = ''
@@ -40,9 +38,10 @@ def decrypt(ciphertext, key):
         plaintext += chars[to.rfind(ch)]
     to = chars
     try:
-        return base64.b64decode(plaintext.encode("ascii")).decode("ascii")[:-1]
+        return base64.b64decode(plaintext.encode("utf-8")).decode("utf-8")
     except:
         raise Exception("failed to decrypt, either key or ciphertext is incorrect")
 
 def customrandom(min, max, seed):
-    return int(math.fabs(math.sin(seed)) * (max - min) + min)
+    return int(math.fabs(math.sin((math.tan(seed * 10 + 1 ) * (max * 2 + 1)) * 3)) * (max - min) + min)
+
